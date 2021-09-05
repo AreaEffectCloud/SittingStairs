a17b8d0f90df91aa5272211a1bc5f518bbe8847e
AreaEffectCloud <84448503+AreaEffectCloud@users.noreply.github.com>
2021-09-05 21:47:22 +0900

    cannot Yml File

--- src/main/java/com/github/areaeffectcloud/sittingstairs/Main.java
+++ src/main/java/com/github/areaeffectcloud/sittingstairs/Main.java
@@ -1,11 +1,7 @@
 package com.github.areaeffectcloud.sittingstairs;
 
-import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public final class Main extends JavaPlugin implements Listener {
 
@@ -13,11 +9,6 @@ public final class Main extends JavaPlugin implements Listener {
     public void onEnable() {
         // Plugin startup logic
         getServer().getPluginManager().registerEvents(new SittingStairs(), this);
-        getServer().getPluginManager().registerEvents(this, this);
-
-        List<String> list = new ArrayList<>();
-        getConfig().set("stairs", list);
-
     }
 
     @Override
