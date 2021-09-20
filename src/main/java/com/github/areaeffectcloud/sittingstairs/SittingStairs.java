12b13c184e92df1cf57dcb0032a22c5a2339f740
AreaEffectCloud <84448503+AreaEffectCloud@users.noreply.github.com>
2021-09-20 13:11:49 +0900

    All Done

--- src/main/java/com/github/areaeffectcloud/sittingstairs/SittingStairs.java
+++ src/main/java/com/github/areaeffectcloud/sittingstairs/SittingStairs.java
@@ -4,33 +4,40 @@ import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.data.Bisected;
 import org.bukkit.block.data.type.Stairs;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 
-import org.bukkit.inventory.ItemStack;
 import org.spigotmc.event.entity.EntityDismountEvent;
 
+import java.io.File;
+import java.util.Collection;
 import java.util.List;
+
+import static org.bukkit.entity.EntityType.ARMOR_STAND;
 
 public class SittingStairs implements Listener {
 
-    public void spawnArmorStand(Block block, Player player) {
+    public void spawnArmorStand(Block block, Player player, double yaw) {
 
         synchronized (this) {
             Location blockLocation = block.getLocation();
 
             double stairsX = blockLocation.getBlockX() + 0.5;
-            double stairsY = blockLocation.getBlockY() - 1.0;
+            double stairsY = blockLocation.getBlockY() - 1.2;
             double stairsZ = blockLocation.getBlockZ() + 0.5;
 
             Location loc = new Location(block.getWorld(), stairsX, stairsY, stairsZ);
+            loc.setYaw((float) yaw);
 
-            Entity armorstand = loc.getWorld().spawnEntity(loc, EntityType.ARMOR_STAND);
+            Entity armorstand = loc.getWorld().spawnEntity(loc, ARMOR_STAND);
             ArmorStand as = (ArmorStand) armorstand;
             as.setInvisible(true);
             armorstand.addPassenger(player);
@@ -45,36 +52,44 @@ public class SittingStairs implements Listener {
         Action action = e.getAction();
         Block block = e.getClickedBlock();
         Player player = e.getPlayer();
+        File stairsYml = new File(Main.plugin.getDataFolder() + File.separator + "stairs.yml");
+        FileConfiguration stairsConfig = YamlConfiguration.loadConfiguration(stairsYml);
+        List<String> list = stairsConfig.getStringList("stairs");
 
         if (action == Action.RIGHT_CLICK_BLOCK) {
             if (player.getInventory().getItemInMainHand().getType().isAir()) {
-                //not doingggg
-                ItemStack item = new ItemStack(Material.getMaterial("stairs"));
-                if (Main.mainclass.getConfig().getString("stairs") == null) {
-                    player.sendMessage(ChatColor.AQUA + Main.mainclass.getConfig().getString("stairs"));
-                }
-                Main.mainclass.getConfig().getConfigurationSection("stairs").getKeys(false).forEach(key -> {
-                    player.sendMessage(ChatColor.GOLD + key);
-                    player.sendMessage(ChatColor.GOLD + "Fase 1st");
-                    if (key.equalsIgnoreCase(block.getType().toString())) {
-                        player.sendMessage(ChatColor.GOLD + "Fase 2nd");
+                for (String key : list) {
+                    if (block.getType() == Material.matchMaterial(key)) {
                         Stairs stairs = (Stairs) block.getBlockData();
                         if (stairs.getHalf() == Bisected.Half.BOTTOM) {
-                            this.spawnArmorStand(block, player);
+                            Collection<Entity> armorstand = block.getWorld().getNearbyEntities(block.getLocation(), 0.5, 1, 0.5, (entity) -> entity.getType() == ARMOR_STAND);
+                            if (armorstand.isEmpty()){
+                                if (stairs.getFacing() == BlockFace.NORTH) {
+                                    double yaw = 0;
+                                    this.spawnArmorStand(block, player, yaw);
+                                } else if (stairs.getFacing() == BlockFace.EAST) {
+                                    double yaw = 90;
+                                    this.spawnArmorStand(block,player, yaw);
+                                } else if (stairs.getFacing() == BlockFace.SOUTH) {
+                                    double yaw = 180;
+                                    this.spawnArmorStand(block, player, yaw);
+                                } else if (stairs.getFacing() == BlockFace.WEST) {
+                                    double yaw = 270;
+                                    this.spawnArmorStand(block, player, yaw);
+                                }
+                            }
                         }
                     }
-                });
+                }
             }
         }
     }
 
     @EventHandler
     public void Remove(EntityDismountEvent e) {
-        if (e.getDismounted().getType() == EntityType.ARMOR_STAND) {
+        if (e.getDismounted().getType() == ARMOR_STAND) {
             ArmorStand armorStand = (ArmorStand) e.getDismounted();
             armorStand.remove();
-
-
         }
     }
 }
